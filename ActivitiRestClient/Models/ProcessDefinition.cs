ed7c27ef3c6a6f5912d2eeb45df540c3cbc0750c
jyamashita <j_yamashita@kure.co.jp>
2014-04-27 19:54:28 +0900

    初期化

---
+++ ActivitiRestClient/Models/ProcessDefinition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivitiRestClient.Models
+{
+    class ProcessDefinition
+    {
+    }
+}
